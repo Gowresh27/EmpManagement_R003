970069f0f81e1fe55ef0d8224814a4ffec5c7b6e
gowresh2701@gmail.com <gowresh2701@gmail.com>
2025-01-23 11:01:14 +0530

    First Commit

---
+++ SpringBootRestEmployeeManagementApp/src/main/java/com/example/demo/entity/Employee.java
@@ -0,0 +1,25 @@
+package com.example.demo.entity;
+
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.Table;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Employee {
+	@Id
+	private int empId;
+	private String empName;
+	private int empSal;
+	private String empDesg;
+	private Date empDOJ;
+
+}
